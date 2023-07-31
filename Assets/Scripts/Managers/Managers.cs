2eb8e11f690a1b5a9b17ee8bdd03d76f8b3a4e3d
Lee HyeChang <guyus5594@gmail.com>
2023-07-31 21:58:17 +0900

    매니저 생성

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance; // 유일성이 보장
+    public static Managers Instance { get { Init();  return s_instance; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Instance 가 null 인 상황에 대한 처리 로직
+    static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+}

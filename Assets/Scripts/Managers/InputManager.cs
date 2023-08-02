b15d29ce5133ef818a96f0d3b0a22b703ed35bd7
Lee HyeChang <guyus5594@gmail.com>
2023-08-02 22:09:53 +0900

    코드 설명 추가

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class InputManager
 {
-    // Define delegates and events
+    // 입력에 따른 이벤트 정의
     public Action OnSpaceDown;
     public Action OnLeftShiftDown;
     public Action OnLeftShift;
     public Action OnLeftMouseDown;
     public Action OnLeftMouseUp;
 
+    // 프레임마다 입력 상태를 확인하고 이벤트를 실행하는 메서드
     public void OnUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Space)) OnSpaceDown?.Invoke();
--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -1,29 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-    static Managers s_instance; // 유일성이 보장
-    static Managers Instance { get { Init();  return s_instance; } }
+    // 싱글턴 패턴을 위한 인스턴스 변수
+    static Managers s_instance;
+    // 싱글턴 패턴의 인스턴스 접근자
+    static Managers Instance { get { Init(); return s_instance; } }
 
+    // 입력 관리자 인스턴스 생성
     InputManager _input = new InputManager();
+    // 입력 관리자 접근자
     public static InputManager Input { get { return Instance._input; } }
 
-    // Start is called before the first frame update
+    // 게임 시작 전 최초 한 번만 실행되는 메서드
     void Start()
     {
         Init();
     }
 
-    // Update is called once per frame
+    // 프레임마다 실행되는 메서드
     void Update()
     {
         _input.OnUpdate();
     }
 
-    // Instance 가 null 인 상황에 대한 처리 로직
+    // 싱글턴 인스턴스 초기화 메서드
     static void Init()
     {
         if (s_instance == null)
@@ -36,6 +37,7 @@ public class Managers : MonoBehaviour
                 go.AddComponent<Managers>();
             }
 
+            // 씬 전환 시 파괴되지 않게 설정
             DontDestroyOnLoad(go);
             s_instance = go.GetComponent<Managers>();
         }

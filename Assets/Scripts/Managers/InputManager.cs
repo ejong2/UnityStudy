fedf205fe86729ef7129dcb9e47ec5b809cc6c20
Lee HyeChang <guyus5594@gmail.com>
2023-08-02 22:12:38 +0900

    설명 추가

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -19,4 +19,17 @@ public class InputManager
         if (Input.GetMouseButtonDown(0)) OnLeftMouseDown?.Invoke();
         if (Input.GetMouseButtonUp(0)) OnLeftMouseUp?.Invoke();
     }
+
+    /**
+    Input.GetMouseButtonDown(0)에서 0은 마우스의 왼쪽 버튼을 나타냅니다. Unity에서 마우스 버튼은 다음과 같이 인덱싱됩니다:
+
+    0: 왼쪽 버튼
+    1: 오른쪽 버튼
+    2: 중간 버튼 (휠 클릭)
+
+    따라서 Input.GetMouseButtonDown(0)은 왼쪽 마우스 버튼이 눌렸을 때 첫 프레임에서 true를 반환합니다.
+    비슷하게, Input.GetMouseButtonUp(0)은 왼쪽 마우스 버튼이 놓였을 때 첫 프레임에서 true를 반환합니다.
+
+    이 코드의 의도는 왼쪽 마우스 버튼의 다양한 상태 (눌렀을 때, 놓았을 때 등)에 따라 관련된 이벤트를 호출하는 것입니다.
+    */
 }
